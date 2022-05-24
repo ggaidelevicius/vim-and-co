bdeaed76311f895bd4f7867834fa4ceb4f3f0141
Gus Gaidelevicius <ggaidelevicius@gmail.com>
2022-05-24 19:36:04 +0800

    download updater

--- gui.py
+++ gui.py
@@ -4,8 +4,16 @@ import pandas as pd
 import csv
 import webbrowser
 from datetime import date
+import requests
 
-print('Vim & Co. website inventory updater\nby gus\nggaidelevicius@gmail.com\n\n')
+print('Vim & Co. website inventory updater\nby gus\nggaidelevicius@gmail.com\n')
+response = requests.get(
+    'https://api.github.com/repos/ggaidelevicius/vim-and-co/releases/latest').json()
+print(f'{response["tag_name"]}\n')
+
+isCurrentVersion = response["tag_name"] == 'v1.0.0'
+downloadLink = response["assets"][0]["browser_download_url"]
+
 # main begin #
 dict_arr = []
 
@@ -69,93 +77,112 @@ canvas = Canvas(
 )
 canvas.place(x=0, y=0)
 
+print(isCurrentVersion)
+if isCurrentVersion:
+    WEB_EXPORT = ''
+    STOCK_EXPORT = ''
 
-WEB_EXPORT = ''
-STOCK_EXPORT = ''
+
+    def select_file(type):
+        global WEB_EXPORT, STOCK_EXPORT
+        filetypes = [('CSV', '*.csv')]
+        filename = filedialog.askopenfilename(
+            title='select afile',
+            initialdir='./',
+            filetypes=filetypes
+        )
+        if type == "web_export":
+            WEB_EXPORT = filename
+        else:
+            STOCK_EXPORT = filename
+        print(f"{type}: {filename}")
+        return filename
 
 
-def select_file(type):
-    global WEB_EXPORT, STOCK_EXPORT
-    filetypes = [('CSV', '*.csv')]
-    filename = filedialog.askopenfilename(
-        title='select afile',
-        initialdir='./',
-        filetypes=filetypes
+    def button_pressed(button):
+        if button == "web_export":
+            web_export_button.config(text=select_file(button))
+        else:
+            inventory_management_export.config(text=select_file(button))
+
+
+    web_export_button = Button(
+        text='Click me to select the website export CSV',
+        command=lambda: button_pressed("web_export"),
+        borderwidth=0,
+        highlightthickness=0,
+        relief="flat"
     )
-    if type == "web_export":
-        WEB_EXPORT = filename
-    else:
-        STOCK_EXPORT = filename
-    print(f"{type}: {filename}")
-    return filename
 
+    web_export_button.place(
+        x=40.0,
+        y=65,
+        width=420.0,
+        height=57.0
+    )
 
-def button_pressed(button):
-    if button == "web_export":
-        web_export_button.config(text=select_file(button))
-    else:
-        inventory_management_export.config(text=select_file(button))
+    inventory_management_export = Button(
+        text='Click me to select the inventory management export CSV',
+        command=lambda: button_pressed("inventory_management_export"),
+        borderwidth=0,
+        highlightthickness=0,
+        relief="flat"
+    )
 
+    inventory_management_export.place(
+        x=40.0,
+        y=132.0,
+        width=420.0,
+        height=57.0
+    )
 
-web_export_button = Button(
-    text='Click me to select the website export CSV',
-    command=lambda: button_pressed("web_export"),
-    borderwidth=0,
-    highlightthickness=0,
-    relief="flat"
-)
+    create_csv = Button(
+        bg='#d5fcbd',
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: processButton(WEB_EXPORT, STOCK_EXPORT),
+        relief="flat",
+        text='Create the updated stock file CSV',
+        font='arial 11 bold'
+    )
+    create_csv.place(
+        x=40.0,
+        y=219.0,
+        width=420.0,
+        height=57.0
+    )
 
-web_export_button.place(
-    x=40.0,
-    y=65,
-    width=420.0,
-    height=57.0
-)
+    def send_help():
+        error_string = ''.join(ch for ch in str(ERROR) if ch.isalnum() or ch == ' ').replace(' ', '%20')
+        webbrowser.open(f'https://ggaidelevicius.com/redirect?subject=the%20inventory%20updater%20is%20broken%20again&body={error_string}', new=2)
 
-inventory_management_export = Button(
-    text='Click me to select the inventory management export CSV',
-    command=lambda: button_pressed("inventory_management_export"),
-    borderwidth=0,
-    highlightthickness=0,
-    relief="flat"
-)
+    help_button = Button(
+        borderwidth=0,
+        highlightthickness=0,
+        relief="flat",
+        text='Click me to contact Gus for help',
+        font='arial 8 bold',
+        bg='#8cddfe',
+        command=send_help
+    )
 
-inventory_management_export.place(
-    x=40.0,
-    y=132.0,
-    width=420.0,
-    height=57.0
-)
+else:
+    update_available = Button(
+        bg='#d5fcbd',
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: webbrowser.open(downloadLink),
+        relief="flat",
+        text='⚡Update available. Click here to download⚡',
+        font='arial 11 bold'
+    )
 
-create_csv = Button(
-    bg='#d5fcbd',
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: processButton(WEB_EXPORT, STOCK_EXPORT),
-    relief="flat",
-    text='Create the updated stock file CSV',
-    font='arial 11 bold'
-)
-create_csv.place(
-    x=40.0,
-    y=219.0,
-    width=420.0,
-    height=57.0
-)
-
-def send_help():
-    error_string = ''.join(ch for ch in str(ERROR) if ch.isalnum() or ch == ' ').replace(' ', '%20')
-    webbrowser.open(f'https://ggaidelevicius.com/redirect?subject=the%20inventory%20updater%20is%20broken%20again&body={error_string}', new=2)
-
-help_button = Button(
-    borderwidth=0,
-    highlightthickness=0,
-    relief="flat",
-    text='Click me to contact Gus for help',
-    font='arial 8 bold',
-    bg='#8cddfe',
-    command=send_help
-)
+    update_available.place(
+        x=40.0,
+        y=142.0,
+        width=420.0,
+        height=57.0
+    )
 
 
 window.resizable(False, False)
